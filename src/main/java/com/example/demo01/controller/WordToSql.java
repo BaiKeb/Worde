054107537778f5fe956801c1b06522ed7f9c19af
2529525329 <2529525329@qq.com>
2022-06-23 11:59:02 +0800

    整理

--- src/main/java/com/example/demo01/controller/WordToSql.java
+++ src/main/java/com/example/demo01/controller/WordToSql.java
@@ -10,29 +10,13 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 
-import org.apache.poi.hwpf.HWPFDocument;
-import org.apache.poi.hwpf.usermodel.CharacterRun;
-import org.apache.poi.hwpf.usermodel.Paragraph;
-import org.apache.poi.hwpf.usermodel.Range;
-import org.apache.poi.poifs.filesystem.POIFSFileSystem;
 import org.apache.poi.xwpf.usermodel.*;
 
 public class WordToSql {
 
-//    public static int sar_id;
-//    public static String air_type;
-//    public static String air_open_type;
-//    public static String air_temp;
-//    public static String run_type;
-//    public static String air_model;
-//    public static String frame;
-//    public static String remark;
-//    public static String wind_direction;
-//    public static String wind_speed;
 
     public static void readAndWriterTesta4() throws IOException {
         File file = new File("D:\\chengtianjiliang\\码值\\码值模板(海尔).docx");
-//        String str = "";
 
         FileInputStream fis = new FileInputStream(file);
         System.out.println("获取到：" + fis);
@@ -40,60 +24,17 @@ public class WordToSql {
 
         Iterator<IBodyElement> bodyEle = xdoc.getBodyElementsIterator();
         List<Object> datas = new ArrayList<>();
-//        String datas1 = String.valueOf(datas);
-        Bean bean = new Bean();
-        Dao dao = new Dao();
+
         while (bodyEle.hasNext()) {
             IBodyElement bodyElement = bodyEle.next();
             String content = handlerByBodyType(bodyElement, bodyElement.getPartType());
-//            if (content == null || content.equals("")){
-//                printAllDatas(datas);
-//            }
+
             datas.add(content);
         }
 
-//        Bean bean = new Bean();
-//        Dao dao = new Dao();
-//        for (Object contents : datas) {
-//            String content = contents.toString();
-//            System.out.println("第一次获取："+content.toString());
-//            System.out.println("第一步:"+content);
-//            if (content.contains("cmode")) {
-//
-//                bean.setAir_type("华凌");
-//                bean.setAir_model("0168");
-//                System.out.println("可以获取" + content.substring(6, 8));
-//                bean.setRun_type(content.substring(6, 8));
-//            }
-//
-//            if (content.contains("conoff")) {
-//                System.out.println("可以获取" + content.substring(7, 8));
-//                bean.setAir_open_type(content.substring(7, 8));
-//            }
-//            if (content.contains("ctemp")) {
-//                System.out.println("可以获取" + content.substring(6, 8));
-//                bean.setAir_temp(content.substring(6, 8));
-//            }
-//            if (content.contains("cwind")) {
-//                System.out.println("可以获取" + content.substring(6, 8));
-//                bean.setWind_speed(content.substring(6, 8));
-//            }
-//            if (content.contains("direction")) {
-//                String str = content.substring(0, content.indexOf(":"));
-//                String str1 = content.substring(str.length() + 1, content.length());
-//                System.out.println("可以获取" + str1);
-//                bean.setWind_direction(str1);
-//            }
-//            if (content.contains("irdata")) {
-//                String str = content.substring(0, content.indexOf(":"));
-//                String str1 = content.substring(str.length() + 1, content.length());
-//                System.out.println("可以获取" + str1);
-//                bean.setFrame(str1);
-//            }
-//        }
+
         xdoc.close();
         fis.close();
-//        dao.add(bean);
         printAllDatas(datas);
 
     }
@@ -166,43 +107,7 @@ public class WordToSql {
         BodyElementType elementType = xwpfParagraph.getElementType();
         String content = getStringByBodyElementType(xwpfParagraph, elementType);
         System.out.println("当前文本的内容为:" + content);
-//        Bean bean = new Bean();
-//        Dao dao = new Dao();
-//        for (int i = 0;i<content.length();i++){
-//        if (content.contains("cmode")) {
-//
-//            bean.setAir_type("华凌");
-//            bean.setAir_model("0168");
-//            System.out.println("可以获取" + content.substring(6, 8));
-//            bean.setRun_type(content.substring(6, 8));
-//        }
-//        }
-//        if (content.contains("conoff")) {
-//            System.out.println("可以获取" + content.substring(7, 8));
-//            bean.setAir_open_type(content.substring(7, 8));
-//        }
-//        if (content.contains("ctemp")) {
-//            System.out.println("可以获取" + content.substring(6, 8));
-//            bean.setAir_temp(content.substring(6, 8));
-//        }
-//        if (content.contains("cwind")){
-//            System.out.println("可以获取"+content.substring(6,8));
-//            bean.setWind_speed(content.substring(6,8));
-//        }
-//        if (content.contains("direction")){
-//            System.out.println("可以获取"+content.substring(10,12));
-//            bean.setWind_direction(content.substring(10,12));
-//        }
-//        if (content.contains("irdata")){
-//            String str = content.substring(0,content.indexOf(":"));
-//            String str1 = content.substring(str.length()+1,content.length());
-//            System.out.println("可以获取"+ str1);
-//            bean.setFrame(str1);
-////            dao.add(bean);
-//        }
 
-
-//        dao.add(bean);
         return content;
     }
 
@@ -253,7 +158,6 @@ public class WordToSql {
 
 
     public static void main(String[] args) throws IOException {
-        Bean bean = new Bean();
         readAndWriterTesta4();
     }
 }
